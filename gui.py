e0cb8e9d9405917eca4bc83c0805f86e9c2ac787
alyssadmoore <alyssadmoore@gmail.com>
2017-03-27 17:42:47 -0500

    Committing so Clara can help me

--- gui.py
+++ gui.py
@@ -1,10 +1,103 @@
 from tkinter import *
-from tkinter import ttk
 import info_processing
-
-root = Tk()
-root.title("Coin Collection Helper")
 
 data = info_processing.json_load()
 
-root.mainloop()
+
+class GUI(Frame):
+    # Initialize frame in a canvas, set scrollbars
+    def __init__(self, root):
+        Frame.__init__(self, root)
+        self.canvas = Canvas(root, borderwidth=0)
+        self.frame = Frame(self.canvas)
+        self.vbar = Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        self.hbar = Scrollbar(root, orient="horizontal", command=self.canvas.xview)
+        self.canvas.configure(yscrollcommand=self.vbar.set, xscrollcommand=self.hbar.set)
+        self.vbar.pack(side="right", fill="y")
+        self.hbar.pack(side="bottom", fill="x")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4, 50), window=self.frame, anchor="nw", tags="self.frame")
+        self.frame.bind("<Configure>", self.on_frame_configure)
+        self.main()
+
+    def quit(self):
+        root.quit()
+
+    # Reset scroll frame to encompass inner frame
+    def on_frame_configure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    # When a picture of a coin is clicked, a new window pops up with more detailed information
+    def new_window(self, num):
+        w = Toplevel(root)
+        newtext = Text(w)
+        newtext.insert(END, "Design description: " + data[num]["description"] + "\n")
+        newtext.insert(END, "Date issued: " + data[num]["date_issued"])
+        newtext.pack(side=LEFT, fill=BOTH)
+
+    def column1(self):
+        for x in range(10):
+            Label(self.frame, text=str(x + 1) + ": " + data[x]["state"]).grid(row=x, column=0)
+            Button(self.frame, image=self.images[x], command=lambda a=x: self.new_window(a), borderwidth=2).grid(row=x, column=1)
+            Label(self.frame, text="Collected?", borderwidth=2).grid(row=x, column=2)
+            self.selection = BooleanVar()
+            check = Checkbutton(self.frame, name="checkbutton" + str(x), variable=self.selection, borderwidth=2)
+            check.grid(row=x, column=3)
+
+    def column2(self):
+        for x in range(10):
+            Label(self.frame, text=str(x + 11) + ": " + data[x + 10]["state"]).grid(row=x, column=4)
+            Button(self.frame, image=self.images[x + 10], command=lambda a=x+10: self.new_window(a), borderwidth=2).grid(row=x, column=5)
+            Label(self.frame, text="Collected?", borderwidth=2).grid(row=x, column=6)
+            self.selection = BooleanVar()
+            check = Checkbutton(self.frame, name="checkbutton" + str(x), variable=self.selection, borderwidth=2)
+            check.grid(row=x, column=7)
+
+    def column3(self):
+        for x in range(10):
+            Label(self.frame, text=str(x + 21) + ": " + data[x + 20]["state"]).grid(row=x, column=8)
+            Button(self.frame, image=self.images[x + 20], command=lambda a=x+20: self.new_window(a), borderwidth=2).grid(row=x, column=9)
+            Label(self.frame, text="Collected?", borderwidth=2).grid(row=x, column=10)
+            self.selection = BooleanVar()
+            check = Checkbutton(self.frame, name="checkbutton" + str(x), variable=self.selection, borderwidth=2)
+            check.grid(row=x, column=11)
+
+    def column4(self):
+        for x in range(10):
+            Label(self.frame, text=str(x + 31) + ": " + data[x + 30]["state"]).grid(row=x, column=12)
+            Button(self.frame, image=self.images[x + 30], command=lambda a=x+30: self.new_window(a), borderwidth=2).grid(row=x, column=13)
+            Label(self.frame, text="Collected?", borderwidth=2).grid(row=x, column=14)
+            self.selection = BooleanVar()
+            check = Checkbutton(self.frame, name="checkbutton" + str(x), variable=self.selection, borderwidth=2)
+            check.grid(row=x, column=15)
+
+    def column5(self):
+        for x in range(10):
+            Label(self.frame, text=str(x + 41) + ": " + data[x + 40]["state"]).grid(row=x, column=16)
+            Button(self.frame, image=self.images[x + 40], command=lambda a=x+40: self.new_window(a), borderwidth=2).grid(row=x, column=17)
+            Label(self.frame, text="Collected?", borderwidth=2).grid(row=x, column=18)
+            self.selection = BooleanVar()
+            check = Checkbutton(self.frame, name="checkbutton" + str(x), variable=self.selection, borderwidth=2)
+            check.grid(row=x, column=19)
+
+    # Setup
+    def main(self):
+        root.title("Coin Collection Helper")
+        self.selection = BooleanVar()
+        self.images = []
+        self.buttons = []
+        self.vars = []
+        for x in range(50):
+            photo = PhotoImage(file="images/coin" + str(x) + ".gif")
+            self.images.append(photo)
+
+        self.column1()
+        # self.column2()
+        # self.column3()
+        # self.column4()
+        # self.column5()
+
+if __name__ == "__main__":
+    root = Tk()
+    gui = GUI(root)
+    root.mainloop()
--- info_processing.py
+++ info_processing.py
@@ -1,16 +1,19 @@
 import re
 import json
+import os
+import urllib.request
 
-
-# Returns state names in the order in the document
-def get_states():
-    all_states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+all_states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
               "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
               "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
               "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
               "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
               "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
               "Wisconsin", "Wyoming"]
+
+
+# Returns state names in the order in the document
+def get_states():
     states_reordered = []
     with open('quarter_info.txt', 'r', encoding='utf8') as f:
         for line in f:
@@ -33,27 +36,39 @@ def get_dates():
 
 
 # Returns each coin's design description
-# TODO many design descriptions are on multiple lines and don't have a universal stop character. How to deal with this?
+# TODO many design descriptions are on multiple lines and don't have a universal stop character. Must deal with this
 def get_descriptions():
+    years = ["1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007"]
     descriptions = []
+    final = []
+    description = ""
     with open('quarter_info.txt', 'r', encoding='utf8') as f:
-        for line in f:
-            if 'Design: ' in line:
-                description = re.findall("Design: (.*) .", line, re.DOTALL)
-                descriptions.append(description[0])
+        try:
+            for line in f:
+                if 'Design: ' in line:
+                    description = (re.findall("Design: (.*) ", line, re.DOTALL)) + (re.findall("\s(\w+)$", line, re.DOTALL))
+                    nextline = next(f).strip()
+                    while (nextline.title() not in all_states) and (nextline not in years):
+                        description.append(nextline)
+                        nextline = next(f).strip()
+                    descriptions.append(description)
+        except:
+            descriptions.append(description)
 
-    return descriptions
+    for x in descriptions:
+        concat = ' '.join(x)
+        final.append(concat)
 
-link_start = "https://www.usmint.gov/images/mint_programs/50sq_program/states/"
-link_end = "_Designs.gif"
-states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA",
-          "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
-          "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    return final
 
 
 # Returns a list of links to each coin's image
-# TODO put in same order as all other info
 def get_links_list():
+    link_start = "https://www.usmint.gov/images/mint_programs/50sq_program/states/"
+    link_end = "_Designs.gif"
+    states = ["DE", "PA", "NJ", "GA", "CT", "MA", "MD", "SC", "NH", "VA", "NY", "NC", "RI", "VT", "KY", "TN", "OH",
+              "LA", "IN", "MS", "IL", "AL", "ME", "MO", "AR", "MI", "FL", "TX", "IA", "WI", "CA", "MN", "OR", "KS",
+              "WV", "NV", "NE", "CO", "ND", "SD", "MT", "WA", "ID", "WY", "UT", "OK", "NM", "AZ", "AK", "HI"]
     links = []
 
     for x in states:
@@ -63,14 +78,11 @@ def get_links_list():
     return links
 
 
-def get_links_dict():
-    links = {}
-
-    for x in states:
-        new_link = link_start + x + link_end
-        links[x] = new_link
-
-    return links
+# Downloads each image, the links to which are generated from the method get_links_list above
+def download_images():
+    links = get_links_list()
+    for x in range(len(links)):
+        urllib.request.urlretrieve(links[x], "images/coin" + str(x) + ".gif")
 
 
 # Dumps info gathered from the above methods into a JSON file
@@ -87,7 +99,7 @@ def json_dump():
     data = []
     for x in range(50):
         to_add = {"id": ids[x], "state": states[x], "date_issued": dates_issued[x],
-                  "description": "todo", "image": links[x], "collected": "false"}
+                  "description": "todo", "image": links[x], "collected": 0}
         data.append(to_add)
 
     with open('data.json', 'w', encoding='utf8') as f:
